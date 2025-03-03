cd1baf202e800f884867f6522631a5bb1c8634d5
Antoine James <mushow@proton.me>
2025-03-03 16:15:03 +0100

    chore(main): demonstrate adding multiple blocks to the chain

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+mod block;
+mod blockchain;
+
+use crate::blockchain::Blockchain;
+
+fn main() {
+    let mut chain = Blockchain::new(2);
+
+    chain.add_block("Hello, RustyChain!".into());
+    chain.add_block("Another block".into());
+    println!("Blockchain: {:#?}", chain);
+}
